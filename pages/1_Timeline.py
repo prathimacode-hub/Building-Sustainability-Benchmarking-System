84d8be23f08727f8816463fd0506657ee0a84ecf
Prathima Kadari <kadariprathima4@gmail.com>
2022-07-08 23:24:23 +0530

    Update 1_Timeline.py

--- pages/1_Timeline.py
+++ pages/1_Timeline.py
@@ -15,7 +15,7 @@ st.subheader("Overview")
 
 st.markdown(f'Timeline from the Start of the Week: ')
 
-st.markdown('<h2 style=color:#4169e1;">Week 1-2</h1>', unsafe_allow_html=True)
+st.markdown('<h3 style=color:#4169e1;">Week 1-2</h1>', unsafe_allow_html=True)
 
 st.markdown(""" 
 * 
@@ -23,7 +23,7 @@ st.markdown("""
 * 
 """, unsafe_allow_html=True)
 
-st.markdown('<h2 style=color:#4169e1;">Week 3-4</h1>', unsafe_allow_html=True)
+st.markdown('<h3 style=color:#4169e1;">Week 3-4</h1>', unsafe_allow_html=True)
 
 st.markdown(""" 
 * 
