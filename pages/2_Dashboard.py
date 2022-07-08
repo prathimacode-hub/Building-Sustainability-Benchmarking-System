ce71e2b8de5e52a994ca7414bb040f69a7517a4e
Prathima Kadari <kadariprathima4@gmail.com>
2022-07-09 00:56:56 +0530

    Update 2_Dashboard.py

--- pages/2_Dashboard.py
+++ pages/2_Dashboard.py
@@ -63,19 +63,24 @@ if active_tab == SUSTAINABILITY_REPORT_WORD_CLOUD:
  
     #st.subheader('VISUALIZATIONS')
     st.markdown('<h4>Word Cloud of a Modified Sustainability Report</h4>', unsafe_allow_html=True)
-    st.image("word_cloud.png", width=400)
+    st.image("word_cloud.png", width=500)
     st.markdown('<h4>Top Words in Headlines versus Count</h4>', unsafe_allow_html=True)
-    st.image("top_words_in_headlines_versus_count.png", width=400)
+    st.image("top_words_in_headlines_versus_count.png", width=500)
     
     st.subheader('INFERENCES')
  
     st.markdown("""
     <p style="font-size:15px">
     - By observing the word cloud(1st graph) keyword extracted are properties, tenants which give information that the company is a Property Management company.
+    
     - This company has emphasized more on energy consumption which is one of most frequently used word.
+    
     - They have got "green leases" which is a significant keyword. 
-    - Most frequently used word is "Waste"
+    
+    - Most frequently used word is "Waste".
+    
     - These results are for 1 page extraction. 
+    
     """, unsafe_allow_html=True)
     
     st.subheader('SUMMARY')
@@ -83,7 +88,9 @@ if active_tab == SUSTAINABILITY_REPORT_WORD_CLOUD:
     st.markdown("""
     <p style="font-size:15px">
     - Super small font size extraction is giving poor results. Metrics fell <50%.
+    
     - EasyOcr works best for text documents
+    
     """, unsafe_allow_html=True)
     
 #     st.subheader('Raw data')
