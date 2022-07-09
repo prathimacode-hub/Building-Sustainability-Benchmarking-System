64d5d60bf2605e6ba943247630976660f4247e5e
Prathima Kadari <kadariprathima4@gmail.com>
2022-07-09 15:35:58 +0530

    Update 4_AI_Solution.py

--- pages/4_AI_Solution.py
+++ pages/4_AI_Solution.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+
+st.set_page_config(
+    page_title="AI Solution",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.sidebar.header("AI Solution")
+
+st.subheader("Modeling Process")
+st.markdown("""
+*  
+""")
