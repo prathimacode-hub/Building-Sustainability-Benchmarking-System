5f23e3be49d728fb4976a70d8be0ea72d1d50d40
Prathima Kadari <kadariprathima4@gmail.com>
2022-07-09 17:31:56 +0530

    Update 1_Timeline.py

--- pages/1_Timeline.py
+++ pages/1_Timeline.py
@@ -10,7 +10,6 @@ st.sidebar.header("Timeline")
 
 st.subheader("Overview")
 
-
 st.markdown(f'Timeline from the Start of the Week: ')
 
 st.markdown('<h3 style=color:#4169e1;">Week 1-2</h1>', unsafe_allow_html=True)
@@ -18,7 +17,7 @@ st.markdown('<h3 style=color:#4169e1;">Week 1-2</h1>', unsafe_allow_html=True)
 st.markdown(""" 
 * Exploring the provided 500 sustainability reports
 * Extracting the data from saasb.org and analyzing it
-* 
+* Figuring out the approach to preprocess the data
 """, unsafe_allow_html=True)
 
 st.markdown('<h3 style=color:#4169e1;">Week 3-4</h1>', unsafe_allow_html=True)
@@ -26,7 +25,8 @@ st.markdown('<h3 style=color:#4169e1;">Week 3-4</h1>', unsafe_allow_html=True)
 st.markdown(""" 
 * Extracting the data from sustainabilityreports.com and responsibilityreports.com
 * Comparing the websites and working on gathering information out of the reports extracted
+* Generated WordCloud using modified PDF
+* Created Streamlit application to visualize the data and modelling process
 * Reflecting the insights into the dashboard for better understandings
-* 
 """, unsafe_allow_html=True)
 
