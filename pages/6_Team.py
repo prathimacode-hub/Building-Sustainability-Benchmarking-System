ec48a3013e51f05fdfc5424cd8179cb352375ff5
Prathima Kadari <kadariprathima4@gmail.com>
2022-07-09 11:55:32 +0530

    Create 6_Team.py

---
+++ pages/6_Team.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.set_page_config(page_title="Team")
+
+st.sidebar.header("Team")
+
+st.subheader('Collaborators')
+
+st.markdown("""
+[Divya Kamat](https://www.linkedin.com/in/)  
+[Srimonti Dutta](https://www.linkedin.com/in/) 
+[Radhika S](https://www.linkedin.com/in/)   
+[Ritika Gupta](https://www.linkedin.com/in/)  
+[Shivam Parab](https://www.linkedin.com/in/)   
+[Vibash Chandra](https://www.linkedin.com/in/)  
+[Farida Kamal](https://www.linkedin.com/in/)  
+[Prathima Kadari](https://www.linkedin.com/in/prathima-kadari/)  
+[Mulugheta Solomon](https://www.linkedin.com/in/)
+[Saad Salami](https://www.linkedin.com/in/)  
+[Sam Faar](https://www.linkedin.com/in/)  
+[Simon Mackenzie](https://www.linkedin.com/in/) 
+[Ayush Bihani](https://www.linkedin.com/in/)  
+[Esha Priyadarshi](https://www.linkedin.com/in/) 
+[Steven Weld](https://www.linkedin.com/in/)
+[David Elvis](https://www.linkedin.com/in/)
+[Laurenz Tophof](https://www.linkedin.com/in/arvind-kay/)  
+[Carl Terence Valdellon](https://www.linkedin.com/in/arvind-kay/)  
+""")
+
+st.subheader('Product Lead')
+
+st.markdown('[Saisreenath Srinivasan Govindarajan](https://www.linkedin.com/in/saisreenath/)')
