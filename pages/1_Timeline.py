b34673e9bd3077d772c5df3586e295a6ba0b4291
Prathima Kadari <kadariprathima4@gmail.com>
2022-07-09 20:23:29 +0530

    Update 1_Timeline.py

--- pages/1_Timeline.py
+++ pages/1_Timeline.py
@@ -18,14 +18,17 @@ st.markdown("""
 * Exploring the provided 500 sustainability reports
 * Extracting the data from saasb.org and analyzing it
 * Figuring out the approach to preprocess the data
+* Extracting the data from sustainabilityreports.com and responsibilityreports.com
 """, unsafe_allow_html=True)
 
 st.markdown('<h3 style=color:#4169e1;">Week 3-4</h1>', unsafe_allow_html=True)
 
 st.markdown(""" 
-* Extracting the data from sustainabilityreports.com and responsibilityreports.com
 * Comparing the websites and working on gathering information out of the reports extracted
 * Generated WordCloud using modified PDF
+* Analyzing the data of sustainability reports.
+* Tabular data was successfully extracted using Camelot and Pytesseract.
+* OCR and ESG bert model gave good efficiency
 * Created Streamlit application to visualize the data and modelling process
 * Reflecting the insights into the dashboard for better understandings
 """, unsafe_allow_html=True)
