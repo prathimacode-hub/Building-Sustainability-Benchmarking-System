0ae636f37ccbe7d8eb269e961b49a7d33db87793
Prathima Kadari <kadariprathima4@gmail.com>
2022-07-09 15:32:41 +0530

    Create 5_Conclusion.py

---
+++ pages/5_Conclusion.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Conclusion",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.sidebar.header("Conclusion")
+
+st.subheader("Conclusion")
+st.markdown("""
+*  
+""")
